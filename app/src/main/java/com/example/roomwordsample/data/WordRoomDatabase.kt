a3282376b6c1574e4fcaac8868497bfdf0705f7f
kotomoto <jkotor@gmail.com>
2019-09-13 16:30:48 +0200

    Dependencies, database classes and word repository

---
+++ app/src/main/java/com/example/roomwordsample/data/WordRoomDatabase.kt
@@ -0,0 +1,35 @@
+package com.example.roomwordsample.data
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = arrayOf(Word::class), version = 1)
+abstract class WordRoomDatabase : RoomDatabase() {
+
+    abstract fun wordDao(): WordDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: WordRoomDatabase? = null
+
+        fun getDatabase(context: Context): WordRoomDatabase {
+            val tempInstance = INSTANCE
+
+            if (tempInstance != null) {
+                return tempInstance
+            }
+
+            synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    WordRoomDatabase::class.java,
+                    "word_database"
+                ).build()
+                INSTANCE = instance
+                return instance
+            }
+        }
+    }
+}
